34169cc9da035e0e6e48b24921c0df0b87c137f6
Bas Kaal <3askaal@gmail.com>
2022-03-26 18:39:16 +0100

    progress on randomized gallery

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-import { times, random } from 'lodash'
+import { random, sample, forEach } from 'lodash'
 
 export default {
   layout: 'home',
@@ -19,21 +19,22 @@ export default {
       const galleryEl = this.$refs.body.querySelector('.gallery--home');
 
       let canvasWidth = this.$refs.body.getBoundingClientRect().width
-      let canvasHeight = 1000
+      let canvasHeight = 800
 
       const images = Array.prototype.slice.call(this.$refs.body.querySelectorAll('.wp-block-image img'))
       const placedImages = []
 
-      const getCorners = (image) => {
+      const getPositions = (image) => {
         return {
           topLeft: { x: image.x, y: image.y },
           topRight: { x: image.x + image.width, y: image.y },
           bottomLeft: { x: image.x, y: image.y + image.height },
           bottomRight: { x: image.x + image.width, y: image.y + image.height },
+          middle: { x: image.x + (image.width / 2), y: image.y + (image.height / 2) }
         }
       }
 
-      const calcRandomImagePositions = (imageWidth, imageHeight) => {
+      const calcRandomImagePosition = (imageWidth, imageHeight) => {
         let randomX = random(0, (canvasWidth - imageWidth))
         let randomY = random(0, (canvasHeight - imageHeight))
         let randomRotate = random(-30, 30)
@@ -48,35 +49,60 @@ export default {
       }
 
       const imageDoesntFit = (image) => {
-        const imageCorners = getCorners(image)
+        const imageCorners = getPositions(image)
 
         return placedImages.some((placedImage) => {
-          const placedImageCorners = getCorners(placedImage)
+          const placedImageCorners = getPositions(placedImage)
 
           return Object.values(imageCorners).some((corner) => {
-            return corner.x > placedImageCorners.topLeft.x &&
-              corner.y > placedImageCorners.topLeft.y &&
-              corner.y < placedImageCorners.bottomRight.y &&
-              corner.x < placedImageCorners.bottomRight.x;
+            return corner.x > (placedImageCorners.topLeft.x - 10) &&
+              corner.y > (placedImageCorners.topLeft.y - 10) &&
+              corner.y < (placedImageCorners.bottomRight.y + 10) &&
+              corner.x < (placedImageCorners.bottomRight.x + 10);
           })
         })
       }
 
-      images.forEach((image) => {
+      const formatSrcSet = (srcset, maxWidth) => {
+        return srcset.split(',')
+          .map((srcSetItem) => {
+            return srcSetItem.trim().split(' ')
+          })
+          .filter(([srcUrl, width]) => {
+            return srcUrl.match(/(\d)+(x)(\d)+/g) && !srcUrl.includes('scaled')
+          })
+          .filter(([srcUrl]) => {
+            const dimensions = srcUrl.match(/(\d)+(x)(\d)+/g)[0].split('x')
+            return dimensions.every((dimension) => Number(dimension) < maxWidth)
+          })
+          .map(([srcUrl]) => srcUrl)
+      }
+
+      forEach(images, (image) => {
         let imageWidth = image.getAttribute('width')
         let imageHeight = image.getAttribute('height')
+        // console.log(image.srcset)
+        const randomSizedImageUrl = sample(formatSrcSet(image.srcset, 500))
+        console.log(randomSizedImageUrl)
 
-        // imageWidth = imageWidth < 500 ? imageWidth : 500
-        // imageHeight = imageHeight < 500 ? imageHeight : 500
+        const dimensions = randomSizedImageUrl.match(/(\d)+(x)(\d)+/g)[0].split('x')
+
+        console.log(dimensions)
         imageWidth = 200
         imageHeight = 200
 
-        let randomPositionedImage = calcRandomImagePositions(imageWidth, imageHeight)
+        console.log(imageWidth, imageHeight)
 
-        console.log(imageDoesntFit(randomPositionedImage))
-        while (imageDoesntFit(randomPositionedImage)) {
-          console.log('imageDoesntFit')
-          randomPositionedImage = calcRandomImagePositions(imageWidth, imageHeight)
+        let randomPositionedImage = calcRandomImagePosition(imageWidth, imageHeight)
+        let tries = 0
+
+        while (imageDoesntFit(randomPositionedImage) && tries < 100) {
+          randomPositionedImage = calcRandomImagePosition(imageWidth, imageHeight)
+          tries++
+        }
+
+        if (tries === 100) {
+          return
         }
 
         image.style.position = 'absolute';
@@ -105,7 +131,6 @@ export default {
 
 .body {
   /* position: relative; */
-
 }
 
 .wp-block-image {
@@ -116,7 +141,6 @@ export default {
   position: relative;
   width: 100%;
   min-height: 800px;
-  border: 1px solid red;
 
   .wp-block-gallery {
     display: flex;
